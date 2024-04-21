80f74a09e539ddc456cedfce047e62d95aaa374e
MarshalLeeeeee <marshallee413lmc@sina.com>
2024-04-21 18:29:37 +0800

    Mod: implement pressure solution (still debugging)

--- src/bin/sim_main.rs
+++ src/bin/sim_main.rs
@@ -1,6 +1,6 @@
-use nalgebra as na;
-use std::time;
-use std::thread;
+use ndarray as nd;
+use sprs::{CsMat, CsVec, TriMat};
+use sprs_ldl::*;
 use fluid_sim::parser;
 use fluid_sim::canvas;
 
@@ -15,221 +15,236 @@ fn main() {
     let width = canvas.get_width();
     let height = canvas.get_height();
 
-    let dt = 0.1_f64;
-    let gravity: na::Vector2<f64> = na::Vector2::new(0_f64, 10_f64);
-    let rho_air = 1.0;
-    let color_air = canvas::RGBAColor::new(0, 0, 0, 255);
-    let rho_smoke = 0.8;
-    let color_smoke = canvas::RGBAColor::new(0, 255, 0, 255);
-    let mut density = na::DMatrix::<f64>::from_fn(
-        width,
-        height,
-        |i, j| { 
-            if i > 40 && i < 60 && j > 40 && j < 60 { 1_f64 }
-            else { 0_f64 }
-        }
-    );
-    let mut velocity_x = na::DMatrix::<f64>::from_fn(width+1, height, |i, j| { 0_f64 });
-    let mut velocity_y = na::DMatrix::<f64>::from_fn(width, height+1, |i, j| { 0_f64 });
+    let dt = 0.01_f64;
+    let g = nd::Array1::<f64>::from_vec(vec![0_f64, 1_f64]);
+    let mut d = nd::Array2::<f64>::from_shape_fn((width, height), |(i, j)|{
+        if 5 <= i && i <= 10 && 2 <= j && j <= 4 { 1_f64 }
+        else { 0_f64 }
+    });
+    let mut vx = nd::Array2::<f64>::from_elem((width+1, height), 0_f64);
+    let mut vy = nd::Array2::<f64>::from_elem((width, height+1), 0_f64);
+    let c_air = canvas::RGBAColor::new(0, 0, 0, 255);
+    let c_fluid = canvas::RGBAColor::new(0, 255, 0, 255);
+    let rho_air = 1_f64;
+    let rho_fluid = 0.5_f64;
 
     while canvas.is_valid() {
-        (velocity_x, velocity_y) = advection(&density, rho_air, rho_smoke, velocity_x, velocity_y, &gravity, dt);
-        density = update_density(density, &velocity_x, &velocity_y, dt);
-        let buffer = update_buffer(&density, &color_air, &color_smoke);
+        let rho = nd_solve_rho(&d, rho_air, rho_fluid);
+        let (fx, fy) = nd_solve_force(&rho, &g, rho_air);
+        nd_velocity_advection(&mut vx, &mut vy, fx, fy, dt);
+        nd_fix_velocit_boundary(&mut vx, &mut vy);
+        nd_pressure_projection(&mut vx, &mut vy, &rho);
+        nd_fix_velocit_boundary(&mut vx, &mut vy);
+        nd_density_advection(&mut d, &vx, &vy, dt);
+        let buffer = na_to_buffer(&d, &c_air, &c_fluid);
         canvas.refresh(&buffer);
     }
 }
 
-fn velocity_x_split(velocity_x: &na::DMatrix::<f64>) -> (na::DMatrix::<f64>, na::DMatrix::<f64>) {
-    let (velocity_x_width, velocity_x_height) = velocity_x.shape();
-    let velocity_x_h = velocity_x.view((1,0), (velocity_x_width-1, velocity_x_height));
-    let velocity_x_l = velocity_x.view((0,0), (velocity_x_width-1, velocity_x_height));
-    (velocity_x_h.into(), velocity_x_l.into())
+fn jagged_x_split(d: &nd::Array2::<f64>) -> (nd::Array2::<f64>, nd::Array2::<f64>) {
+    let d0 = d.slice(nd::s![..-1, ..]);
+    let d1 = d.slice(nd::s![1.., ..]);
+    (d0.to_owned(), d1.to_owned())
 }
 
-fn velocity_x_gradient(velocity_x: &na::DMatrix::<f64>) -> na::DMatrix::<f64> {
-    let (velocity_x_h, velocity_x_l) = velocity_x_split(&velocity_x);
-    let mut velocity_x_g = (velocity_x_h - velocity_x_l).insert_row(0, 0_f64);
-    let velocity_x_g_r1 = velocity_x_g.row(1).into_owned();
-    velocity_x_g.set_row(0, &velocity_x_g_r1);
-    velocity_x_g
+fn jagged_x_gradient(d: &nd::Array2::<f64>) -> nd::Array2::<f64> {
+    let (d0, d1) = jagged_x_split(d);
+    d1 - d0
 }
 
-fn velocity_y_split(velocity_y: &na::DMatrix::<f64>) -> (na::DMatrix::<f64>, na::DMatrix::<f64>) {
-    let (velocity_y_width, velocity_y_height) = velocity_y.shape();
-    let velocity_y_h = velocity_y.view((0,1), (velocity_y_width, velocity_y_height-1));
-    let velocity_y_l = velocity_y.view((0,0), (velocity_y_width, velocity_y_height-1));
-    (velocity_y_h.into(), velocity_y_l.into())
+fn jagged_x_gradient_jagged(d: &nd::Array2::<f64>) -> nd::Array2::<f64> {
+    let (d0, d1) = jagged_x_split(d);
+    let dg = d1 - d0;
+    let dg_x0 = dg.slice(nd::s![0..1, ..]).to_owned();
+    let mut dgg = nd::Array2::zeros(d.dim());
+    dgg.slice_mut(nd::s![0..1, ..]).assign(&dg_x0);
+    dgg.slice_mut(nd::s![1.., ..]).assign(&dg);
+    dgg
 }
 
-fn velocity_y_gradient(velocity_y: &na::DMatrix::<f64>) -> na::DMatrix::<f64> {
-    let (velocity_y_h, velocity_y_l) = velocity_y_split(&velocity_y);
-    let mut velocity_y_g = (velocity_y_h - velocity_y_l).insert_column(0, 0_f64);
-    let velocity_y_g_c1 = velocity_y_g.column(1).into_owned();
-    velocity_y_g.set_column(0, &velocity_y_g_c1);
-    velocity_y_g
+fn to_jagged_x(d: &nd::Array2::<f64>) -> nd::Array2::<f64> {
+    let (w, h) = d.dim();
+    let (d0, d1) = jagged_x_split(d);
+    let d_mid = (d0 + d1) * 0.5;
+    let mut d_new = nd::Array2::<f64>::from_elem((w+1,h), 0_f64);
+    d_new.slice_mut(nd::s![..1, ..]).assign(&d.slice(nd::s![..1, ..]).to_owned());
+    d_new.slice_mut(nd::s![1..-1, ..]).assign(&d_mid);
+    d_new.slice_mut(nd::s![-1.., ..]).assign(&d.slice(nd::s![-1.., ..]).to_owned());
+    d_new
 }
 
-/// convert (w,h) to (w+1, h)
-fn to_edged_x(data: &na::DMatrix::<f64>) -> na::DMatrix::<f64> {
-    let (data_width, data_height) = data.shape();
-    let data_h = data.view((1,0), (data_width-1, data_height));
-    let data_l = data.view((0,0), (data_width-1, data_height));
-    let mut data_mid = (data_h + data_l) * 0.5;
-    data_mid = data_mid.insert_row(0, 0_f64);
-    data_mid.set_row(0, &data.row(0).into_owned());
-    data_mid = data_mid.insert_row(data_width, 0_f64);
-    data_mid.set_row(data_width, &data.row(data_width-1).into_owned());
-    data_mid
+fn jagged_y_split(d: &nd::Array2::<f64>) -> (nd::Array2::<f64>, nd::Array2::<f64>) {
+    let d0 = d.slice(nd::s![.., ..-1]);
+    let d1 = d.slice(nd::s![.., 1..]);
+    (d0.to_owned(), d1.to_owned())
 }
 
-/// convert (w,h) to (w, h+1)
-fn to_edged_y(data: &na::DMatrix::<f64>) -> na::DMatrix::<f64> {
-    let (data_width, data_height) = data.shape();
-    let data_h = data.view((0,1), (data_width, data_height-1));
-    let data_l = data.view((0,0), (data_width, data_height-1));
-    let mut data_mid = (data_h + data_l) * 0.5;
-    data_mid = data_mid.insert_column(0, 0_f64);
-    data_mid.set_column(0, &data.column(0).into_owned());
-    data_mid = data_mid.insert_column(data_height, 0_f64);
-    data_mid.set_column(data_height, &data.column(data_height-1).into_owned());
-    data_mid
+fn jagged_y_gradient(d: &nd::Array2::<f64>) -> nd::Array2::<f64> {
+    let (d0, d1) = jagged_y_split(d);
+    d1 - d0
 }
 
-fn advection(density: &na::DMatrix::<f64>, rho_air: f64, rho_smoke: f64, velocity_x: na::DMatrix::<f64>, velocity_y: na::DMatrix::<f64>, gravity: &na::Vector2<f64>, dt: f64) -> (na::DMatrix::<f64>, na::DMatrix::<f64>) {
-    let density_x = to_edged_x(density);
-    let density_smoke = density_x.clone() * rho_smoke;
-    let density_air = (density_x.clone()*(-1_f64)).add_scalar(1_f64) * rho_air;
-    let density_mix = density_smoke + density_air;
-    let force_x = density_mix.add_scalar(-rho_air).component_div(&density_mix) * gravity.y;
+fn jagged_y_gradient_jagged(d: &nd::Array2::<f64>) -> nd::Array2::<f64> {
+    let (d0, d1) = jagged_y_split(d);
+    let dg = d1 - d0;
+    let dg_y0 = dg.slice(nd::s![.., ..1]).to_owned();
+    let mut dgg = nd::Array2::zeros(d.dim());
+    dgg.slice_mut(nd::s![.., ..1]).assign(&dg_y0);
+    dgg.slice_mut(nd::s![.., 1..]).assign(&dg);
+    dgg
+}
+
+fn to_jagged_y(d: &nd::Array2::<f64>) -> nd::Array2::<f64> {
+    let (w, h) = d.dim();
+    let (d0, d1) = jagged_y_split(d);
+    let d_mid = (d0 + d1) * 0.5;
+    let mut d_new = nd::Array2::<f64>::from_elem((w,h+1), 0_f64);
+    d_new.slice_mut(nd::s![.., ..1]).assign(&d.slice(nd::s![.., ..1]).to_owned());
+    d_new.slice_mut(nd::s![.., 1..-1]).assign(&d_mid);
+    d_new.slice_mut(nd::s![.., -1..]).assign(&d.slice(nd::s![.., -1..]).to_owned());
+    d_new
+}
+
+fn nd_solve_rho(d: &nd::Array2::<f64>, rho_air: f64, rho_fluid: f64) -> nd::Array2::<f64> {
+    d * (rho_fluid - rho_air) + rho_air
+}
+
+fn nd_solve_force(rho: &nd::Array2::<f64>, g: &nd::Array1::<f64>, rho_air: f64) -> (nd::Array2::<f64>, nd::Array2::<f64>) {
+    let rho_x = to_jagged_x(&rho);
+    let fx = (rho_air - rho_x.clone()) / rho_x * g[0];
     
-    let (vx_w, vx_h) = velocity_x.shape();
-    let velocity_x_g = velocity_x_gradient(&velocity_x);
-    let mut velocity_x_p = na::DMatrix::<f64>::zeros(vx_w, vx_h);
-    let euler_advection_x = force_x * dt - velocity_x_g * dt;
-    velocity_x.add_to(&euler_advection_x, &mut velocity_x_p);
-    let velocity_x_p_g = velocity_x_gradient(&velocity_x_p);
-    velocity_x_p = (velocity_x + velocity_x_p) * 0.5 - velocity_x_p_g * 0.5 * dt;
-    
-    let density_y = to_edged_y(density);
-    let density_smoke = density_y.clone() * rho_smoke;
-    let density_air = (density_y.clone()*(-1_f64)).add_scalar(1_f64) * rho_air;
-    let density_mix = density_smoke + density_air;
-    let force_y = density_mix.add_scalar(-rho_air).component_div(&density_mix) * gravity.x;
-    
-    let (vy_w, vy_h) = velocity_y.shape();
-    let velocity_y_g = velocity_y_gradient(&velocity_y);
-    let mut velocity_y_p = na::DMatrix::<f64>::zeros(vy_w, vy_h);
-    let euler_advection_y = force_y * dt - velocity_y_g * dt;
-    velocity_y.add_to(&euler_advection_y, &mut velocity_y_p);
-    let velocity_y_p_g = velocity_y_gradient(&velocity_y_p);
-    velocity_y_p = (velocity_y + velocity_y_p) * 0.5 - velocity_y_p_g * 0.5 * dt;
+    let rho_y = to_jagged_y(&rho);
+    let fy = (rho_air - rho_y.clone()) / rho_y * g[1];
 
-    (velocity_x_p, velocity_y_p)
+    (fx, fy)
 }
 
-fn pressure_solution(density: &na::DMatrix::<f64>, velocity: &na::DMatrix::<na::Vector2<f64>>, force: &na::Vector2<f64>) {
-    // TODO: A linear system solver
-    // let sz = 1000;
-    // let r = na::DMatrix::<f64>::from_fn(sz, sz, |i, j| {
-    //     if rand::random::<f64>() > 0.9_f64 { rand::random::<f64>() }
-    //     else { 0_f64 }
-    // });
-    // let mut b = na::DMatrix::<f64>::zeros(sz, 1);
-    // b = b.add_scalar(1_f64);
-
-    // let lu = na::LU::new(r);
-    // if let Some(x) = lu.solve(&b) {
-    //     println!("ok!");
-    //     // println!("{}", x);
-    // }
-
-    // let qr = na::QR::new(r);
-    // if let Some(x) = qr.solve(&b) {
-    //     println!("ok!");
-    //     // println!("{}", x);
-    // }
-
-    // let fplu = na::FullPivLU::new(r);
-    // if let Some(x) = fplu.solve(&b) {
-    //     println!("ok!");
-    //     // println!("{}", x);
-    // }
-
-
-    // use sprs::{CsMat, TriMat, SpSolver};
-    // use sprs::SprsShape;
-    // // 创建一个高维稀疏矩阵和一个向量b
-    // let matrix_a = CsMat::new((3, 3), vec![0, 2, 3], vec![0, 1, 2], vec![4.0, 5.0, 6.0]);
-    // let vector_b = vec![1.0, 2.0, 3.0];
-
-    // // 解线性系统 Ax=b
-    // let solver = SpSolver::factorize(&matrix_a).unwrap();
-    // let solution = solver.solve(&vector_b).unwrap();
-    // println!("Solution: {:?}", solution);
-}
-
-fn projection(density: &na::DMatrix::<f64>, velocity: &na::DMatrix::<na::Vector2<f64>>, force: &na::Vector2<f64>) {
-    // TODO: apply projection
-    // TODO: process boundary velocity
-}
-
-fn update_density(density: na::DMatrix::<f64>, velocity_x: &na::DMatrix::<f64>, velocity_y: &na::DMatrix::<f64>, dt: f64) -> na::DMatrix::<f64> {
-    let (velocity_x_h, velocity_x_l) = velocity_x_split(&velocity_x);
-    let velocity_x_mid = (velocity_x_h + velocity_x_l) * 0.5;
-    let dx = velocity_x_mid * dt;
+fn nd_velocity_advection(vx: &mut nd::Array2::<f64>, vy: &mut nd::Array2::<f64>, force_x: nd::Array2::<f64>, force_y: nd::Array2::<f64>, dt: f64) {
+    let vx_g = jagged_x_gradient_jagged(&vx);
+    let vx_pre = vx.clone() + force_x * dt - vx_g * dt;
+    let vx_pre_g = jagged_x_gradient_jagged(&vx_pre);
+    let vx_new = (vx.clone() + vx_pre) * 0.5 - vx_pre_g * 0.5 * dt;
+    vx.assign(&vx_new);
     
-    let (velocity_y_h, velocity_y_l) = velocity_y_split(&velocity_y);
-    let velocity_y_mid = (velocity_y_h + velocity_y_l) * 0.5;
-    let dy = velocity_y_mid * dt;
+    let vy_g = jagged_y_gradient_jagged(&vy);
+    let vy_pre = vy.clone() + force_y * dt - vy_g * dt;
+    let vy_pre_g = jagged_y_gradient_jagged(&vy_pre);
+    let vy_new = (vy.clone() + vy_pre) * 0.5 - vy_pre_g * 0.5 * dt;
+    vy.assign(&vy_new);
+}
 
-    let (density_width, density_height) = density.shape();
-    let mut new_density = na::DMatrix::zeros(density_width, density_height);
-    for i in 0..density_width {
-        for j in 0..density_height {
-            let index = i*density_width+j;
+fn nd_fix_velocit_boundary(vx: &mut nd::Array2::<f64>, vy: &mut nd::Array2::<f64>) {
+    let (w, h) = vx.dim();
+    let w = w - 1;
+    for j in 0..h {
+        if vx[[0, j]] < 0_f64 { vx[[0, j]] = 0_f64; }
+        if vx[[w-1, j]] > 0_f64 { vx[[w-1, j]] = 0_f64; }
+    }
+    for i in 0..w {
+        if vy[[i, 0]] < 0_f64 { vy[[i, 0]] = 0_f64; }
+        if vy[[i, h-1]] > 0_f64 { vy[[i, h-1]] = 0_f64; }
+    }
+}
 
-            let ii = i as f64 - dx.get(index).unwrap();
-            let jj = j as f64 - dy.get(index).unwrap();
-            let ii0 = ii.floor() as usize;
-            let ii1 = ii.ceil() as usize;
-            let jj0 = jj.floor() as usize;
-            let jj1 = jj.ceil() as usize;
+fn nd_solve_velocity_divengence(vx: &nd::Array2::<f64>, vy: &nd::Array2::<f64>) -> nd::Array2::<f64> {
+    jagged_x_gradient(&vx) + jagged_y_gradient(&vy)
+}
 
-            let d00 = get_density_by_index(&density, ii0, jj0);
-            let d01 = get_density_by_index(&density, ii0, jj1);
-            let d10 = get_density_by_index(&density, ii1, jj0);
-            let d11 = get_density_by_index(&density, ii1, jj1);
-            let d0 = d00 + (d01 - d00) * (ii - ii0 as f64);
-            let d1 = d10 + (d11 - d10) * (ii - ii0 as f64);
-            let d = d0 + (d1 - d0) * (jj - jj0 as f64);
+fn nd_pressure_projection(vx: &mut nd::Array2::<f64>, vy: &mut nd::Array2::<f64>, rho: &nd::Array2::<f64>) {
+    let (w, h) = rho.dim();
+    let sz = w * h;
 
-            if let Some(v) = new_density.get_mut(index) {
-                *v = d.max(0_f64).min(1_f64);
+    let divergence = nd_solve_velocity_divengence(&vx, &vy);
+    let mut pa = TriMat::<f32>::new((sz, sz));
+    let mut pbi = Vec::with_capacity(sz);
+    let mut pbv: Vec<f32> = Vec::with_capacity(sz);
+    for i in 0..w {
+        for j in 0..h {
+            let mut cnt = 0;
+            if i > 0 {
+                pa.add_triplet(i*h+j, (i-1)*h+j, -1_f32);
+                cnt += 1;
             }
+            if i < w-1 {
+                pa.add_triplet(i*h+j, (i+1)*h+j, -1_f32);
+                cnt += 1;
+            }
+            if j > 0 {
+                pa.add_triplet(i*h+j, i*h+j-1, -1_f32);
+                cnt += 1;
+            }
+            if j < h-1 {
+                pa.add_triplet(i*h+j, i*h+j+1, -1_f32);
+                cnt += 1;
+            }
+            pa.add_triplet(i*h+j, i*h+j, cnt as f32);
+
+            pbi.push(i*h+j);
+            pbv.push((divergence[[i, j]] * rho[[i, j]]) as f32);
         }
     }
+    let pa: CsMat<_> = pa.to_csr();
+    let pb = CsVec::new(sz, pbi, pbv);
+    let ldl = Ldl::default();
+    let system = ldl.numeric(pa.view()).unwrap();
+    let p = system.solve(pb.to_dense());
+    let p = nd::Array2::<f64>::from_shape_fn((w, h), |(i,j)|{
+        p[i*h+j] as f64
+    });
 
-    new_density
+    // pressure projection
+    let px = to_jagged_x(&p);
+    let pxg = jagged_x_gradient_jagged(&px);
+    *vx = vx.clone() + pxg / to_jagged_x(&rho);
+    
+    let py = to_jagged_y(&p);
+    let pyg = jagged_y_gradient_jagged(&py);
+    *vy = vy.clone() + pyg / to_jagged_y(&rho);
 }
 
-fn get_density_by_index(density: &na::DMatrix::<f64>, i: usize, j: usize) -> f64 {
-    let (density_width, density_height) = density.shape();
-    if i >= density_width { 0_f64 }
-    else if j >= density_height { 0_f64 }
-    else {
-        match density.get(i * density_width + j) {
-            Some(v) => *v,
-            None => 0_f64,
+fn nd_density_advection(d: &mut nd::Array2::<f64>, vx: &nd::Array2::<f64>, vy: &nd::Array2::<f64>, dt: f64) {
+    let (w, h) = d.dim();
+    let xs = nd::Array2::<f64>::from_shape_fn((w, h), |(i, _j)|{ i as f64 });
+    let ys = nd::Array2::<f64>::from_shape_fn((w, h), |(_i, j)|{ j as f64 });
+
+    let (vx_0, vx_1) = jagged_x_split(&vx);
+    let vx_mid = (vx_0 + vx_1) * 0.5_f64;
+    let dx = vx_mid * dt;
+    
+    let (vy_0, vy_1) = jagged_y_split(&vy);
+    let vy_mid = (vy_0 + vy_1) * 0.5_f64;
+    let dy = vy_mid * dt;
+    
+    let dxs = (xs - dx).map(|o|{ o.clamp(0_f64, w as f64 - 1_f64) });
+    let dys = (ys - dy).map(|o|{ o.clamp(0_f64, h as f64 - 1_f64) });
+    
+    let dxs0 = dxs.map(|o|{ o.floor() });
+    let dxs1 = dxs.map(|o|{ o.ceil() });
+    let dys0 = dys.map(|o|{ o.floor() });
+    let dys1 = dys.map(|o|{ o.ceil() });
+    
+    let dx0y0 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs0[[i,j]] as usize, dys0[[i,j]] as usize]] });
+    let dx0y1 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs0[[i,j]] as usize, dys1[[i,j]] as usize]] });
+    let dx1y0 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs1[[i,j]] as usize, dys0[[i,j]] as usize]] });
+    let dx1y1 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs1[[i,j]] as usize, dys1[[i,j]] as usize]] });
+    
+    let ddxs = dxs - dxs0;
+    let ddys = dys - dys0;
+    
+    let dxy0 = (dx1y0 - dx0y0.clone()) * ddxs.clone() + dx0y0;
+    let dxy1 = (dx1y1 - dx0y1.clone()) * ddxs + dx0y1;
+    let dxy = (dxy1 - dxy0.clone()) * ddys + dxy0;
+
+    d.assign(&dxy);
+}
+
+fn na_to_buffer(d: &nd::Array2::<f64>, c_air: &canvas::RGBAColor, c_fluid: &canvas::RGBAColor) -> Vec<canvas::RGBAColor> {
+    let (width, height) = d.dim();
+    let mut buffer = Vec::with_capacity(width * height);
+    for j in 0..height {
+        for i in 0..width {
+            buffer.push(canvas::RGBAColor::mix(&c_fluid, &c_air, d[[i, j]]));
         }
-    }
-}
-
-fn update_buffer(density: &na::DMatrix::<f64>, color_air: &canvas::RGBAColor, color_smoke: &canvas::RGBAColor) -> Vec<canvas::RGBAColor> {
-    let (density_width, density_height) = density.shape();
-    let mut buffer = Vec::with_capacity(density_width * density_height);
-    for d in density.iter() {
-        buffer.push(color_smoke.mix(*d, color_air));
     }
     buffer.reverse();
     buffer
