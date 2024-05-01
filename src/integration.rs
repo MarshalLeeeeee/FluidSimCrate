57c3e381a03ccaf090c298983132254b53c1f735
MarshalLeeeeee <marshallee413lmc@sina.com>
2024-05-01 19:42:14 +0800

    Add: integration document

--- src/integration.rs
+++ src/integration.rs
@@ -2,7 +2,21 @@
 use ndarray as nd;
 
 pub trait Integrate {
+
+    /// Get the incremental part for integration
+    ///
+    /// Params:
+    /// - ```d```: data to be integrated.
+    /// - ```dd```: the first derivative of ```d```.
+    /// - ```step```: discrete step for integration.
     fn get_integration_step(&self, d: &nd::Array2::<f64>, dd: &nd::Array2::<f64>, step: f64) -> nd::Array2::<f64>;
+    
+    /// Get the new data after integration
+    ///
+    /// Params:
+    /// - ```d```: data to be integrated.
+    /// - ```dd```: the first derivative of ```d```.
+    /// - ```step```: discrete step for integration.
     fn integrate(&self, d: &nd::Array2::<f64>, dd: &nd::Array2::<f64>, step: f64) -> nd::Array2::<f64> {
         d + self.get_integration_step(&d, &dd, step)
     }
@@ -15,10 +29,23 @@ impl Integrate for ForwardEuler {
     }
 }
 
+/// Get the incremental part for integration
+///
+/// Params:
+/// - ```integ```: specific integration type.
+/// - ```d```: data to be integrated.
+/// - ```dd```: the first derivative of ```d```.
+/// - ```step```: discrete step for integration.
 pub fn get_integration_step(integ: Box<dyn Integrate>, d: &nd::Array2::<f64>, dd: &nd::Array2::<f64>, step: f64) -> nd::Array2::<f64> {
     integ.get_integration_step(&d, &dd, step)
 }
 
+/// Get the new data after integration
+///
+/// Params:
+/// - ```d```: data to be integrated.
+/// - ```dd```: the first derivative of ```d```.
+/// - ```step```: discrete step for integration.
 pub fn integrate(integ: Box<dyn Integrate>, d: &nd::Array2::<f64>, dd: &nd::Array2::<f64>, step: f64) -> nd::Array2::<f64> {
     integ.integrate(&d, &dd, step)
 }
