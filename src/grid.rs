054cc6cc8fe5e9bb22c2d25e2c92c44857b94499
MarshalLeeeeee <marshallee413lmc@sina.com>
2024-05-01 13:06:16 +0800

    Add: new api to sample data with spatial offset

--- src/grid.rs
+++ src/grid.rs
@@ -55,3 +55,30 @@ pub fn to_staggered_y_grid(d: &nd::Array2::<f64>, boundary: Box<dyn boundary::Bo
     d_new.slice_mut(nd::s![.., -1..]).assign(&boundary.boundary(&d.column(h-1).to_owned()).insert_axis(nd::Axis(1)));
     d_new
 }
+
+pub fn sample_with_offset(d: &nd::Array2::<f64>, dx: nd::Array2::<f64>, dy: nd::Array2::<f64>) -> nd::Array2::<f64> {
+    let (w, h) = d.dim();
+    let xs = nd::Array2::<f64>::from_shape_fn((w, h), |(i, _j)|{ i as f64 });
+    let ys = nd::Array2::<f64>::from_shape_fn((w, h), |(_i, j)|{ j as f64 });
+    
+    let dxs = (xs - dx).map(|o|{ o.clamp(0_f64, w as f64 - 1_f64) });
+    let dys = (ys - dy).map(|o|{ o.clamp(0_f64, h as f64 - 1_f64) });
+    
+    let dxs0 = dxs.map(|o|{ o.floor() });
+    let dxs1 = dxs.map(|o|{ o.ceil() });
+    let dys0 = dys.map(|o|{ o.floor() });
+    let dys1 = dys.map(|o|{ o.ceil() });
+    
+    let dx0y0 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs0[[i,j]] as usize, dys0[[i,j]] as usize]] });
+    let dx0y1 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs0[[i,j]] as usize, dys1[[i,j]] as usize]] });
+    let dx1y0 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs1[[i,j]] as usize, dys0[[i,j]] as usize]] });
+    let dx1y1 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ d[[dxs1[[i,j]] as usize, dys1[[i,j]] as usize]] });
+    
+    let ddxs = dxs - dxs0;
+    let ddys = dys - dys0;
+    
+    let dxy0 = (dx1y0 - dx0y0.clone()) * ddxs.clone() + dx0y0;
+    let dxy1 = (dx1y1 - dx0y1.clone()) * ddxs + dx0y1;
+    let dxy = (dxy1 - dxy0.clone()) * ddys + dxy0;
+    dxy
+}
--- src/scene.rs
+++ src/scene.rs
@@ -149,10 +149,6 @@ impl SingleSmokeGridScene {
     }
     
     fn _density_advection(&mut self) {
-        let (w, h) = self.sf_d.dim();
-        let xs = nd::Array2::<f64>::from_shape_fn((w, h), |(i, _j)|{ i as f64 });
-        let ys = nd::Array2::<f64>::from_shape_fn((w, h), |(_i, j)|{ j as f64 });
-    
         let (vx_0, vx_1) = split_staggered_x_grid(&self.vf_vx);
         let vx_mid = (vx_0 + vx_1) * 0.5_f64;
         let dx = vx_mid * self.dt;
@@ -160,28 +156,8 @@ impl SingleSmokeGridScene {
         let (vy_0, vy_1) = split_staggered_y_grid(&self.vf_vy);
         let vy_mid = (vy_0 + vy_1) * 0.5_f64;
         let dy = vy_mid * self.dt;
-        
-        let dxs = (xs - dx).map(|o|{ o.clamp(0_f64, w as f64 - 1_f64) });
-        let dys = (ys - dy).map(|o|{ o.clamp(0_f64, h as f64 - 1_f64) });
-        
-        let dxs0 = dxs.map(|o|{ o.floor() });
-        let dxs1 = dxs.map(|o|{ o.ceil() });
-        let dys0 = dys.map(|o|{ o.floor() });
-        let dys1 = dys.map(|o|{ o.ceil() });
-        
-        let dx0y0 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ self.sf_d[[dxs0[[i,j]] as usize, dys0[[i,j]] as usize]] });
-        let dx0y1 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ self.sf_d[[dxs0[[i,j]] as usize, dys1[[i,j]] as usize]] });
-        let dx1y0 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ self.sf_d[[dxs1[[i,j]] as usize, dys0[[i,j]] as usize]] });
-        let dx1y1 = nd::Array2::<f64>::from_shape_fn((w, h), |(i, j)|{ self.sf_d[[dxs1[[i,j]] as usize, dys1[[i,j]] as usize]] });
-        
-        let ddxs = dxs - dxs0;
-        let ddys = dys - dys0;
-        
-        let dxy0 = (dx1y0 - dx0y0.clone()) * ddxs.clone() + dx0y0;
-        let dxy1 = (dx1y1 - dx0y1.clone()) * ddxs + dx0y1;
-        let dxy = (dxy1 - dxy0.clone()) * ddys + dxy0;
-    
-        self.sf_d.assign(&dxy);
+
+        self.sf_d.assign(&sample_with_offset(&self.sf_d, dx, dy));
     }
 }
 impl Scene for SingleSmokeGridScene {
