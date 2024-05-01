402bce8a2cf76cd5bacd585bc466c7b02159184b
MarshalLeeeeee <marshallee413lmc@sina.com>
2024-05-01 14:08:33 +0800

    Mod: reorganize examples and treat the implementation of scene as the external of this crate

--- src/scene_examples/single_smoke_sim.rs
+++ src/scene_examples/single_smoke_sim.rs
@@ -5,13 +5,13 @@ use fluid_sim::scene;
 fn main() {
     let raw_parser_maps = vec!(
         canvas::Canvas::get_config_map(),
-        scene::SingleSmokeGridScene::get_config_map(),
+        scene::single_smoke::SingleSmokeGridScene::get_config_map(),
     );
     let parser_map = parser::register(raw_parser_maps);
     let parser = parser::parse(parser_map);
 
     let mut canvas = canvas::Canvas::new_by_parser(&parser);
-    let mut scene = scene::SingleSmokeGridScene::new_by_parser(&parser);
+    let mut scene = scene::single_smoke::SingleSmokeGridScene::new_by_parser(&parser);
 
     while canvas.is_valid() {
         scene.sim();
