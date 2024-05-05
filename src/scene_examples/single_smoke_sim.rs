ebe51adf09b0b3e03acd6bc09cd59b5dfb9b0e01
MarshalLeeeeee <marshallee413lmc@sina.com>
2024-05-05 12:15:48 +0800

    Mod: add example cmd for bin in comment

--- src/scene_examples/single_smoke_sim.rs
+++ src/scene_examples/single_smoke_sim.rs
@@ -2,6 +2,10 @@ use fluid_sim::parser;
 use fluid_sim::canvas;
 use fluid_sim::scene;
 
+/// Run with 
+/// cargo run --bin single_smoke_sim -- json_file=./configs/single_smoke.json [...]
+///
+/// Different configs can be put in [...], or change the corresponding json file
 fn main() {
     let raw_parser_maps = vec!(
         canvas::Canvas::get_config_map(),
